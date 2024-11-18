99eaab62acc633f3401e16ca7035aa97b139bb85
adrianknoo <adrian.cano.navarro@estudiantat.upc.edu>
2024-11-18 21:59:24 +0100

    trying the airfoils

--- main.m
+++ main.m
@@ -1,14 +1,16 @@
 clear; close all
+%% Read data: %%
+[NACA_Data, nodes_NACA] = read_data('NACA0015');
 
 %% Geometry & initialization: %%
 D = 2;
-N = 128;
+N = nodes_NACA(3)-1; %128 cylinder; nodes_NACA(3)
 Q_inf = [1,0];
 kuttaChange = ceil(N / 4);
 geometry = 'given';
 
 %% Called functions: %%
-[beta, x_nod, z_nod, x_c_glob, z_c_glob, l_j, normals, tangents] = discretize_geometry(D, N,'NACA0015');
+[distance, x_nod, z_nod, x_c_glob, z_c_glob, l_j, normals, tangents] = discretize_geometry(D, N,NACA_Data,'readf');
 [a,b, ~] = precompute_terms(normals, tangents, N, Q_inf, x_c_glob, z_c_glob, x_nod,z_nod, l_j);
 [a,b] = applyKuttaCondition(kuttaChange, a, b);
 gammas = solveVortexStrength(a,b, kuttaChange);
@@ -22,37 +24,37 @@ plot(x_c_glob, z_c_glob, '-o');
 %hold on
 axis equal
 grid on
-ylim([-1 1])
+%ylim([-1 1])
 %plot(x_nod, z_nod, '-o');
 
 % Plot velocity versus theta:
 figure;
-plot(rad2deg(beta(1:end-1)), V(:,1))
+plot(distance(1:end-1), V(:,1))
 hold on
 grid on
-xlim([0 360])
-plot(rad2deg(beta(1:end-1)), V(:,2))
+xlim([0 1])
+plot(distance(1:end-1), V(:,2))
 title('Distribution of velocity','FontSize',18,'Interpreter','latex')
 xlabel('$\theta$ [$^\circ$]','FontSize',16,'Interpreter','latex')
 ylabel('$V_x$ and $V_z$ [m/s]','FontSize',16,'Interpreter','latex')
 
 % Plot cp versus theta:
 figure;
-plot(rad2deg(beta(1:end-1)), cp(1,:))
+plot(distance(1:end-1), cp(1,:))
 hold on
 grid on
-xlim([0 360])
-ylim([-3 1])
+xlim([0 1])
+%ylim([-3 1])
 title('Distribution of pressure coefficient','FontSize',18,'Interpreter','latex')
 xlabel('$\theta$ [$^\circ$]','FontSize',16,'Interpreter','latex')
 ylabel('$C_p$ [-]','FontSize',16,'Interpreter','latex')
 
 % Plot gamma versus theta:
 figure;
-plot(rad2deg(beta(1:end-1)), gammas(:,1))
+plot(distance(1:end-1), gammas(:,1))
 hold on
 grid on
-xlim([0 360])
+xlim([0 1])
 title('Distribution of $\gamma$','FontSize',18,'Interpreter','latex')
 xlabel('$\theta$ [$^\circ$]','FontSize',16,'Interpreter','latex')
 ylabel('$\gamma$ [-]','FontSize',16,'Interpreter','latex')
--- read_data.m
+++ read_data.m
@@ -1,94 +1,64 @@
-function [NACA_Data, num_nodes_NACA] = read_data(options)
+function [NACA_Data, nodes_NACA] = read_data(options)
 
 % Switch to concatenate tables based on the selected option
 switch options
     case 'NACA0015'
         
-        data1_15=readtable('NACA0015/NACA0015_N_32.txt');
-        data2_15=readtable('NACA0015/NACA0015_N_64.txt');
-        data3_15=readtable('NACA0015/NACA0015_N_128.txt');
-        data4_15=readtable('NACA0015/NACA0015_N_256.txt');
-        data5_15=readtable('NACA0015/NACA0015_N_512.txt');
-
-        % Determine the maximum number of rows
-        maxRows = max([height(data1_15), height(data2_15), height(data3_15), height(data4_15), height(data5_15)]);
+        % Lista de archivos
+        files = {
+            'NACA0015/NACA0015_N_32.txt',
+            'NACA0015/NACA0015_N_64.txt',
+            'NACA0015/NACA0015_N_128.txt',
+            'NACA0015/NACA0015_N_256.txt',
+            'NACA0015/NACA0015_N_512.txt'
+        };
         
-        % Pad each table up to the maximum number of rows with NaN
-        data1 = padarray(table2array(data1_15), [maxRows - height(data1_15), 0], NaN, 'post');
-        data2 = padarray(table2array(data2_15), [maxRows - height(data2_15), 0], NaN, 'post');
-        data3 = padarray(table2array(data3_15), [maxRows - height(data3_15), 0], NaN, 'post');
-        data4 = padarray(table2array(data4_15), [maxRows - height(data4_15), 0], NaN, 'post');
-        data5 = padarray(table2array(data5_15), [maxRows - height(data5_15), 0], NaN, 'post');
-
-        % Convert each matrix back to a table
-        data1 = array2table(data1);
-        data2 = array2table(data2);
-        data3 = array2table(data3);
-        data4 = array2table(data4);
-        data5 = array2table(data5);
-        
-        % Define the tables in a cell array for easy iteration
-        NACA_Data = {data1, data2, data3, data4, data5};
-        
-        % Create a vector to store the number of valid rows in each table
-        num_nodes_NACA = zeros(1, length(NACA_Data));
-        
-        % Loop to count valid rows in the first column of each table
-        for i = 1:length(NACA_Data)
-            % Select the first column of the current table
-            first_column = NACA_Data{i}{:, 1};
-            
-            % Count the rows that are not NaN in the first column
-            num_nodes_NACA(i) = sum(~isnan(first_column));
+        % Leer todos los archivos en una celda
+        NACA_Data = cell(1, length(files));
+        for i = 1:length(files)
+            NACA_Data{i} = readtable(files{i});  % Leer cada archivo y almacenarlo en la celda
         end
         
-        % Display the result
+        % Inicializar un vector para contar los nodos (filas válidas)
+        nodes_NACA = zeros(1, length(NACA_Data));
+        
+        % Contar el número de filas válidas (sin NaN) en la primera columna de cada tabla
+        for i = 1:length(NACA_Data)
+            first_column = NACA_Data{i}{:, 1};  % Seleccionar la primera columna de la tabla
+            nodes_NACA(i) = sum(~isnan(first_column));  % Contar las filas que no tienen NaN
+        end
+        
+        % Mostrar el resultado
         disp('Number of nodes for each case with the NACA 0015:');
-        disp(num_nodes_NACA);
+        disp(nodes_NACA);
 
     case 'NACA22112'
-
-        data1_22=readtable('NACA22112/NACA_22112_N_32.txt');
-        data2_22=readtable('NACA22112/NACA_22112_N_64.txt');
-        data3_22=readtable('NACA22112/NACA_22112_N_128.txt');
-        data4_22=readtable('NACA22112/NACA_22112_N_256.txt');
-        data5_22=readtable('NACA22112/NACA_22112_N_512.txt');
-
-        % Determine the maximum number of rows
-        maxRows = max([height(data1_22), height(data2_22), height(data3_22), height(data4_22), height(data5_22)]);
+         % Lista de archivos
+        files = {
+            'NACA22112/NACA_22112_N_32.txt',
+            'NACA22112/NACA_22112_N_64.txt',
+            'NACA22112/NACA_22112_N_128.txt',
+            'NACA22112/NACA_22112_N_256.txt',
+            'NACA22112/NACA_22112_N_512.txt'
+        };
         
-        % Pad each table up to the maximum number of rows with NaN
-        data1 = padarray(table2array(data1_22), [maxRows - height(data1_22), 0], NaN, 'post');
-        data2 = padarray(table2array(data2_22), [maxRows - height(data2_22), 0], NaN, 'post');
-        data3 = padarray(table2array(data3_22), [maxRows - height(data3_22), 0], NaN, 'post');
-        data4 = padarray(table2array(data4_22), [maxRows - height(data4_22), 0], NaN, 'post');
-        data5 = padarray(table2array(data5_22), [maxRows - height(data5_22), 0], NaN, 'post');
-
-        % Convert each matrix back to a table
-        data1 = array2table(data1);
-        data2 = array2table(data2);
-        data3 = array2table(data3);
-        data4 = array2table(data4);
-        data5 = array2table(data5);
-
-        % Define the tables in a cell array for easy iteration
-        NACA_Data = {data1, data2, data3, data4, data5};
-        
-        % Create a vector to store the number of valid rows in each table
-        num_nodes_NACA = zeros(1, length(NACA_Data));
-        
-        % Loop to count valid rows in the first column of each table
-        for i = 1:length(NACA_Data)
-            % Select the first column of the current table
-            first_column = NACA_Data{i}{:, 1};
-            
-            % Count the rows that are not NaN in the first column
-            num_nodes_NACA(i) = sum(~isnan(first_column));
+        % Leer todos los archivos en una celda
+        NACA_Data = cell(1, length(files));
+        for i = 1:length(files)
+            NACA_Data{i} = readtable(files{i});  % Leer cada archivo y almacenarlo en la celda
         end
         
-        % Display the result
+        % Inicializar un vector para contar los nodos (filas válidas)
+        nodes_NACA = zeros(1, length(NACA_Data));
+        
+        % Contar el número de filas válidas (sin NaN) en la primera columna de cada tabla
+        for i = 1:length(NACA_Data)
+            first_column = NACA_Data{i}{:, 1};  % Seleccionar la primera columna de la tabla
+            nodes_NACA(i) = sum(~isnan(first_column));  % Contar las filas que no tienen NaN
+        end
+        
+        % Mostrar el resultado
         disp('Number of nodes for each case with the NACA 22112:');
-        disp(num_nodes_NACA);
-end
+        disp(nodes_NACA);
 end
 
